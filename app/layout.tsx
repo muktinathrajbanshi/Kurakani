492e84d7e2b745d8107101b5a6ae600123898ca3
unknown <muktinathrajbanshi786@gmail.com>
2023-09-05 07:46:52 +0545

    Complete

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Inter } from 'next/font/google'
+
+import ToasterContext from './context/ToasterContext'
+
+
+import ActiveStatus from './components/ActiveStatus'
+import './globals.css'
+
+import AuthContext from './context/AuthContext'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Kurakani',
+  description: 'Kurakani',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+        <ToasterContext />
+        <ActiveStatus />
+        {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
