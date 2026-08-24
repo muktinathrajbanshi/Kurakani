5d7adfce875c8eb1ea13f5c6ee18e36ae8caea08
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-08-24 22:24:02 +0545

    Update application title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,33 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-import ToasterContext from './context/ToasterContext'
+import ToasterContext from "./context/ToasterContext";
 
+import ActiveStatus from "./components/ActiveStatus";
+import "./globals.css";
 
-import ActiveStatus from './components/ActiveStatus'
-import './globals.css'
+import AuthContext from "./context/AuthContext";
 
-import AuthContext from './context/AuthContext'
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Kurakani',
-  description: 'Kurakani',
-}
+  title: "Muktinath Tech — Chat",
+  description: "Muktinath Tech Chat",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
-        <ToasterContext />
-        <ActiveStatus />
-        {children}
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
         </AuthContext>
       </body>
     </html>
-  )
+  );
 }
